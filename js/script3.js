2b20a4d21e1a5dcc7053ffde2596427d2313d5f5
AntonKashshay0310 <antosha.kashshay@gmail.com>
2025-01-29 19:55:43 +0200

    add

---
+++ js/script3.js
@@ -0,0 +1,19 @@
+const name = 'Anton';
+const surname = 'Kashshay';
+const fullname = `${name} ${surname}`;
+console.log(fullname.toUpperCase());
+console.log(fullname.length);
+console.log(fullname.charAt());
+console.log(fullname.indexOf('t'));
+
+const x = 'John';
+const y = 'Doe';
+const n = 'Hotel';
+const g = 13;
+
+const message = `Гість ${x} ${y} поселяється в ${n} номер ${g}`
+console.log(message);
+
+
+
+
